6c43721f22e826ca2a243abc8f86b3f658ac63f5
keesvanlierop <keesvanlierop@hotmail.com>
2023-11-20 18:40:06 +0100

    Add `@diakem/libre-link-up-api-client`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world')
+import 'dotenv/config'
+import { LibreLinkUpClient } from './libre-link-up-api-client/src'
+
+const run = async () => {
+    if (!process.env.LIBRE_LINK_UP_USERNAME || !process.env.LIBRE_LINK_UP_PASSWORD) {
+        throw new Error('Invalid credentials')
+    }
+
+    const { read } = LibreLinkUpClient({ 
+        username: process.env.LIBRE_LINK_UP_USERNAME, 
+        password: process.env.LIBRE_LINK_UP_PASSWORD 
+    });
+    
+    const response = await read()
+    console.log(response)
+}
+
+run()
