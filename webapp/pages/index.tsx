fe5c8781fa08d47ba1537112b1d946280a8b3287
keesvanlierop <keesvanlierop@hotmail.com>
2023-11-20 20:20:37 +0100

    Some styles

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -3,7 +3,6 @@ import type { GetServerSideProps, InferGetServerSidePropsType } from "next"
 export const getServerSideProps = (async () => {
   const response = await fetch('http://localhost:4000/data')
   const data = await response.json()
-  console.log(data)
   return { props: { data } }
 }) satisfies GetServerSideProps<{
   data: any 
@@ -12,7 +11,7 @@ export const getServerSideProps = (async () => {
 export default function Page({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>{data.current.value || '-'}</h1> 
+      <h1 className="text-7xl">{data.current.value || '-'}</h1> 
     </main>
   )
 }
