f85ed690911376d2901abd28d06fad99acc29d31
Kees van Lierop <keesvanlierop@hotmail.com>
2023-11-24 12:10:50 +0100

    Updated CI

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,10 @@ set -e
 yarn 
 yarn workspace @glukees/webapp build
 yarn workspace @glukees/api build
-forever stop 0
+sudo forever stop 1
+sudo forever stop 0
 sudo kill $(sudo lsof -t -i:3000)
 sudo kill $(sudo lsof -t -i:4000)
-sudo forever start -c 'yarn start' ./
+sudo forever start -c 'yarn workspace @glukees/api start' ./
+sudo forever start -c 'yarn workspace @glukees/webapp start' ./
 sudo forever list
