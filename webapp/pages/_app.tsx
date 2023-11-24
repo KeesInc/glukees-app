c96326667b13efd6f33a55dafc6b4f3b5af85986
Kees van Lierop <keesvanlierop@hotmail.com>
2023-11-24 16:38:22 +0100

    GA

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,11 +1,30 @@
 import { AppProps } from 'next/app'
+import Script from 'next/script'
 import '../app/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  // @ts-expect-error - borked next types
-  return <Component 
-    {...pageProps} 
-  />
+function MyApp({ Component: C, pageProps }: AppProps) {
+  const Component = C as any
+
+  return <>
+    {process.env.GA_MEASUREMENT_ID && (
+      <>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`} />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </>
+    )}
+
+    <Component 
+      {...pageProps} 
+    />
+  </> 
 }
 
 export default MyApp
